d24659f554cc0a0f262a2f9c6f37892873bb31ec
Mark Linderman <marklinderman@Marks-MacBook-Pro.local>
2018-12-09 14:03:44 -0500

    back propogation working

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -21,9 +21,6 @@ Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
 
 Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
                  num_labels, (hidden_layer_size + 1));
-size(Theta1)
-size(Theta2)
-size(X)
 
 % Setup some useful variables
 m = size(X, 1);
@@ -81,21 +78,58 @@ Theta2_grad = zeros(size(Theta2));
 a1 = [ones(m,1), X];
 a2 = [ones(m,1), sigmoid(a1 * transpose(Theta1))];
 a3 = sigmoid(a2 * transpose(Theta2));
-size(a2)
-size(a3)
-size(y)
+
+delta2 = zeros([size(a2, 2)]); % just the column count (nodes - 1 per theta)
+delta3 = zeros([size(a3, 2)]); % ditto
 
 % compute the cost by looping over a3 values and accumulating the 
 % costs for each output node (10 in this case), for each y 
 for i = 1:m 
-    % map y values to a row vector of num_labels length so that you can compare to output nodes while computing cost
-    yvector = zeros(1,num_labels);
-    yvector(1, y(i)) = 1;
+    % map y values to a row vector of num_labels length so that 
+    % you can compare to output nodes while computing cost
+    yvector = zeros(num_labels, 1);
+    yvector(y(i), 1) = 1;
     for j = 1:num_labels
-        % yvector and a3 should both be a row vector of size num_labels
+        % yvector should be a col vector of size num_labels
+        % a3 is 2 dimensional but we're selecting the corresponding
+        % sample row (i) and theta(j) one at a time.
         J += -yvector(j)*log(a3(i,j)) - (1 - yvector(j))*log(1 - a3(i,j));
+
     end
-end
+
+    % vectorized computation of delta3 (the easiest delta to compute)
+    % (delta3 has one element per output node, so 10 elements)
+    % you could also do this above inside the for loop
+    % for each node one at a time (non-vectorized)
+    % since a3 was computed for all samples, it's 5000x10
+    % just take the current sample's row (the current m in the loop)
+    delta3 = transpose(a3(i, :)) - yvector;  % element-wise subtraction
+    % now d3 has deltas for just one sample
+    % and we can back prop to d2 for that sample
+    % transpose Theta2 is a 26x10; and, since you're only 
+    % computing for a single m inside this loop,
+    % you only want a single row from a2 which was computed
+    % for all the samples before entry into this loop.
+    % here's that single row to make this more readable:
+    a2m = a2(i, :); % 1 x 26 since there are 10 output nodes in layer a2
+    a1m = a1(i, :); % 1 X 401 - these are input nodes
+    % transpose(Theta2) is a 26 x 10; delta3 is a 1 x 10 so
+    % that multiplcation should produce a 26 x 10; a2m is a 1 x 26
+    % how will that work with element-wise multiplcation?
+    delta2 = (transpose(Theta2) * delta3) .* transpose(a2m .* (1 - a2m));
+    % now lop off the bias column; there is no bias column
+    % in delta3 because that was calculated from nn outputs a3
+    delta2 = delta2(2:end);
+    % now add these deltas to accumulaters that will be updated for 
+    % every training row m and then used to compute partial 
+    % derivates for all thetas for the network
+    Theta2_grad = Theta2_grad + delta3 * a2m;
+    Theta1_grad = Theta1_grad + delta2 * a1m;
+end 
+
+Theta2_grad = (1/m) .* Theta2_grad;
+Theta1_grad = (1/m) .* Theta1_grad;
+
 
 J = (1/m) * J;
 
@@ -103,8 +137,8 @@ J = (1/m) * J;
 J += (lambda/(2*m)) * (sum(sum(Theta1(:,2:end).^2)) + sum(sum(Theta2(:,2:end).^2)));
 
 
-
-
+% so now we have the  cost of the entire network; it's a single value
+% how does that help?  We want to minimize it.
 
 
 
--- machine-learning-ex4/ex4/randInitializeWeights.m
+++ machine-learning-ex4/ex4/randInitializeWeights.m
@@ -18,12 +18,10 @@ W = zeros(L_out, 1 + L_in);
 %
 % Note: The first column of W corresponds to the parameters for the bias unit
 %
-
-
-
-
-
-
+% i think this initializes W to have random values between 
+% -epsilon and epsilon
+epsilon_init = 0.12;
+W = rand(L_out, 1 + L_in) * 2 * epsilon_init - epsilon_init;
 
 
 
